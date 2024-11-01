58a75c3e586c050ff4b4464c39372a3fbd338b71
nonzoo <nonsoosadebe202@gmail.com>
2024-11-01 11:01:23 +0100

    collected static

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-17%60&rne&3-fob53z5@@(42#f+t_*__3+l7-1r(rp$=r=xgyt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -105,9 +105,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static/"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
 MEDIA_URL = '/media/'
