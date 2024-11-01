56aca4feb2a66441bef2453d7b97f78c986620a5
nonzoo <nonsoosadebe202@gmail.com>
2024-11-01 11:29:57 +0100

    updated the static files pattern

--- project/urls.py
+++ project/urls.py
@@ -3,10 +3,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from portfolio.views import homeview
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homeview, name='homeview')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+
